35ec2db0b05f6916c2f9aa01aa8c40673f7251fb
Wes <wes@miragecorp.org>
2023-12-30 18:13:11 -0600

    moved Flask config to constats.py

--- constants.py
+++ constants.py
@@ -1,1 +1,12 @@
-APP_SECRET = "nischalNEEDSagirlfriend"
+import os 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SECRET_KEY = "nischalNEEDSagirlfriend"
+DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+def format(number):
+    return "-".join(number[i:i+4] for i in range(0, len(number), 4))
